cc5d965fea064ea365dc5e6b2b390acbe2d88653
NikitaAgrawal53 <nikita.agrawal53@gmail.com>
2019-12-20 23:25:40 +0000

    Update Nate_Backend.js

--- Nate_Backend.js
+++ Nate_Backend.js
@@ -19,19 +19,51 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./Nate_Url.html'));
 
 app.post('/send',  function (req, res) {
-	console.log("Inside function",req.body.enter_url);
+  console.log("Inside function",req.body.enter_url);
+  var getData = new Promise((resolve,reject)=>{
   https.get(req.body.enter_url, (res1) =>
   {
     res1.on('data', (value) => {
-      result = value.toString();
+      result = result + value.toString();
       fs.appendFile('res.txt', result, function (err) {
         if (err) throw err;
-        console.log('Saved!');
-      });
-    })
-  })
+       // console.log('Saved!');
+        //resolve(result);
 
-	
+      });
+     // resolve(result);
+    })
+    resolve(result);
+   
+  })
+})
+var count = new Promise((resolve,reject)=>{
+
+
+  fs.readFile('res.txt', function(err, data) {
+    if (err) {
+      throw err;
+    }
+  
+    data = wordcounter.count(data.toString());
+  console.log(data);
+    fs.writeFile('resultnate1.json', JSON.stringify(data), function (err) {
+      if (err) {
+        throw err;
+      }
+  
+      console.log('Done, without errors.');
+    });
+  });
+})
+
+
+getData.then((result) =>{
+  console.log(result);
+  count.then((countres)=>{
+      console.log("Done");
+  })
+})	
 		
 
 	
